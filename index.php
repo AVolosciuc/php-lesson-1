45811202b9a4a4b78f5377244a13da89653d49d0
AVolosciuc <anea.tanas@mail.ru>
2022-12-20 20:37:02 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First PHP File</title>
+</head>
+<body>
+    <?php
+    echo "Date is:" .date ('j-m-y, h:i:s') ."<br>". "<hr>";
+    $color="red";
+    echo "My car is " .$color . "<br>" . "<hr>";
+
+    $txt ="Hello World";
+    echo 'I love $txt!' . "<br>". "<hr>";
+    echo "I love " .$txt . "!" . "<br>". "<hr>";
+
+    $nume = "Anuscuta";
+    echo "Ma numesc $nume". "<br>". "<hr>";
+
+    
+    $a = 10;
+    $b = 15;
+    echo  $a + $b  . "<br>". "<hr>";
+
+    ?>
+    
+   
+
+
+</body>
+</html>
